33b246e6640fcc8d6c2bfd68625a08cf6842876d
Jesse Pledger <JessePledger@gmail.com>
2015-01-16 14:58:17 -0800

    Deleted schema.db file and added belongs_to :user to post.rb to fix db error

--- app/models/post.rb
+++ app/models/post.rb
@@ -7,8 +7,10 @@
 #  body       :text
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
+#  user_id    :integer
 #
 
 class Post < ActiveRecord::Base
   has_many :comments
+  belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -31,4 +31,6 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, 
          :confirmable
+
+  has_many :posts
 end
