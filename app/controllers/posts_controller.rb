dba938cd0a70356d05b9b3a99eb68ec27f1e2295
Jesse Pledger <JessePledger@gmail.com>
2015-01-20 19:59:36 -0800

    Fixed and styled editing posts as Admin.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,7 +5,7 @@ class PostsController < ApplicationController
   end
 
   def new
-    @topic = Topic.new
+    @topic = Topic.find(params[:topic_id])
     @post = Post.new
     authorize @post
   end
@@ -13,12 +13,14 @@ class PostsController < ApplicationController
   def create
     @topic = Topic.find(params[:topic_id])
     @post = current_user.posts.build(params.require(:post).permit(:title, :body))
+    @post.topic = @topic
     authorize @post
+
     if @post.save
       flash[:notice] = "Post was saved."
       redirect_to [@topic, @post]
     else
-      flash[:error] = "There was an error saving the post.  Please try again."
+      flash[:error] = "There was an error saving the post. Please try again."
       render :new
     end
   end
