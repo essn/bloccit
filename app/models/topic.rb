f1cc4cd9dd7ef376e8e73dd9a76f5d6d32ff3ff1
Jesse Pledger <JessePledger@gmail.com>
2015-01-27 23:48:11 -0800

    Added scope and testing for topics.  Implemented logic for public and private topics.

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -13,4 +13,7 @@
 class Topic < ActiveRecord::Base
   has_many :posts, dependent: :destroy
 
+  scope :visible_to, -> (user) { user ? all : publicly_viewable }
+  scope :privately_viewable, -> { where(public: false) }
+  scope :publicly_viewable, -> { where(public: true) }
 end
