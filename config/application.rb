bb1b90174d50f7219191b66859f12deb8e97426e
Jesse Pledger <JessePledger@gmail.com>
2015-04-22 11:53:45 -0700

    Everything seems to be working but account is provisioned.

--- config/application.rb
+++ config/application.rb
@@ -30,5 +30,9 @@ module Bloccit
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    # ActionMailer config
+    config.action_mailer.perform_deliveries = true
+    config.action_mailer.default :charset => "utf-8"
   end
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,6 +77,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  # http://bloccit.herokuapp.com
+  # Mailer settings
   config.action_mailer.default_url_options = { host: 'bloccit-jwp.herokuapp.com' }
+  config.action_mailer.perform_deliveries = true
 end
